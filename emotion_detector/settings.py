edceac38031d7a14370c0a3441fd1630ec17f2d7
auscode <harshittext@gmail.com>
2023-12-11 12:41:10 +0530

    setting for deployemnt

--- emotion_detector/settings.py
+++ emotion_detector/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a_xc5^!1+5s5c#*3wb2y$giwc5d*2t*nyjmm=*zokr&s)3$(fc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
